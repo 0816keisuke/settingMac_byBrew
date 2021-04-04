531bc3f6c0637e8f6bbeca7805efc6d39f6e0463
keisuke.fukada <keisukefukada.0816@gmail.com>
2021-04-04 22:40:33 +0900

    changed comment-out line from ja to en

--- setup.sh
+++ setup.sh
@@ -1,38 +1,42 @@
 #!/bin/bash
 
-# Homebrewの確認
-while read -p "Do You have \"Homebrew?\" y/n: " yn; do
+# Copyright (c) 2021 Keisuke.Fukada (0816keisuke)
+# This software is released under the MIT License, see LICENSE.
+
+# Check Homebrew
+while read -p "Do You have Homebrew? y/n: " yn; do
     if [ $yn = "y" ]; then
-        # brewコマンドのパス確認
+        # Check the path of command "brew"
         which brew > /dev/null
         if [ $? != 0 ]; then
-            echo "Create path 'brew' command. Exit."
+            echo "Command 'brew' is not find. Create path of 'brew'. Exit."
             exit
         fi
-        echo "Continue process."
         break
     elif [ $yn = "n" ]; then
-        echo "Please install Homebrew first. Exit."
+        echo "Install Homebrew and create path of 'brew' command first."
+        echo "You can install from the following URL. Exit."
+        echo "https://brew.sh"
         exit
     else
-        echo "Please Input y or n."
+        echo "Enter y or n."
     fi
 done
 
-# エラー発生時点で終了
-set -e -o pipefail 
-trap 'echo "ERROR: line no = $LINENO, exit status = $?" >&2; exit 1' ERR
+# Terminate execution when an error is occured
+set -e -o pipefail
+trap 'echo "ERROR: line $LINENO, exit status = $?." >&2; exit 1' ERR
 
-# BrewListを読み込み後，改行とコメントアウト行を削除して各行を配列化
+# Read BrewList & delete blank line and comment-out line & make BrewList list
 all_list=(`cat BrewList | grep -vE "^$|#"`)
 
-# --formulaeのスペルチェック
+# Check spelling "--formulae"
 if [ ${all_list[0]} != "--formulae" ]; then
     echo "'--formulae' is NOT written. Exit."
     exit
 fi
 
-# --casksのスペルチェック
+# Check spelling "--casks"
 flag=0
 for ((idx=0; idx<${#all_list[@]}; idx++)); do
     if [ ${all_list[$idx]} == "--casks" ]; then
@@ -48,14 +52,14 @@ fi
 formulae_list=("${all_list[@]}")
 cask_list=("${all_list[@]}")
 
-# インストールするformulaeをリスト化
+# Make installing formulae list
 unset formulae_list[0]
 for ((i=$idx; i<${#all_list[@]}; i++)); do
     unset formulae_list[$i]
 done
 formulae_list=(${formulae_list[@]})
 
-# インストールするcaskをリスト化
+# Make installing casks list
 for ((i=0; i<=$idx; i++)); do
     unset cask_list[$i]
 done
@@ -65,55 +69,54 @@ cask_list=(${cask_list[@]})
 # echo "formulae: ${formulae_list[@]}"
 # echo "    cask: ${cask_list[@]}"
 
-# formulaeのインストール
+# Install formulae
 for formulae in ${formulae_list[@]}; do
-    echo "Installing $formulae..."
+    echo "\\nInstalling $formulae..."
     brew install $formulae
 done
 
-# caskのインストール
+# Install casks
 for cask in ${cask_list[@]}; do
-    echo "Installing $cask..."
+    echo "\\nInstalling $cask..."
     brew install --cask $cask
 done
 
-# iTerm2のカラースキーム(iceberg)のインストール
+# Install color-scheme of iTerm2 (iceberg)
 curl -o ~/Downloads/iceberg.itermcolors -O https://raw.githubusercontent.com/aseom/dotfiles/master/osx/iterm2/iceberg.itermcolors
 
-# gitconfigの作成
+# Create file gitconfig
 if [ ! -e ~/.gitconfig ]; then
     touch ~/.gitconfig
 fi
 
 read -p "Enter your user name on Git: " git_user_name
-read -p "Enter your email address on Git: " git_mail
+read -p "Enter your mail address on Git: " git_mail
 git config --global user.name $git_user_name
 git config --global user.email $git_mail
 
 # .zshrc
 cp ./.zshrc ~/.zshrc
 
-# vimrcの作成
+# Create vimrc
 if [ ! -e ~/.vimrc ]; then
     touch ~/.vimrc
 fi
 
-# 研究室用ディレクトリの作成
-if [ ! -d ~/lab/ ]; then
+# Create lab directory
+if [ ! -d ~/lab ]; then
     mkdir ~/lab
 fi
 
-# Python用ディレクトリの作成
-if [ ! -d ~/python/ ]; then
+# Create Python directory
+if [ ! -d ~/python ]; then
     mkdir ~/python
 fi
 
-# C/C++用ディレクトリの作成
-if [ ! -d ~/c_cpp/ ]; then
+# Create C/C++ directory
+if [ ! -d ~/c_cpp ]; then
     mkdir ~/c_cpp
 fi
 
-# gcc/g++はbrewでのインストール時に"gcc", "g++"で/usr/local/binにPATHが通らないのでシンボリックリンクを作成
 # unlink /usr/local/bin/gcc
 # path=`find /usr/local/Cellar/gcc -name "gcc-10"`
 # ln -s path /usr/local/bin/gcc
@@ -121,27 +124,30 @@ fi
 # path=`find /usr/local/Cellar/gcc -name "g++-10"`
 # ln -s path /usr/local/bin/gcc
 
-# Rust用ディレクトリの作成 & Rustのインストール
+# Create Rust directory & install Rust
 while read -p "Do You install Rust? y/n: " yn; do
     if [ $yn = "y" ]; then
-        if  [ ! -d ~/rust/ ]; then
-            echo HELLO
+        if  [ ! -d ~/rust ]; then
             mkdir ~/rust
         fi
         echo "Start to install Rust..."
-        # curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
         break
     elif [ $yn = "n" ]; then
         echo "Don't install Rust."
         break
     else
-        echo "Please Input y or n."
+        echo "Enter y or n."
     fi
 done
 
-# ssh関連
+# SSH
 if [ ! -d ~/.ssh ]; then
     chmod 755 ~
     mkdir ~/.ssh
     chmod 700 ~/.ssh
+    echo "Generate ssh-key RSA 4096-bit."
+    ssh-keygen -b 4096 -t rsa
 fi
+
+echo "Setting Mac is DONE! Have a good Mac-life!!"
