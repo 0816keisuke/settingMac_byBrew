786ae9c86bd0f8582cb51e396d289cecb0f32d6b
keisuke.fukada <keisukefukada.0816@gmail.com>
2021-05-03 00:56:42 +0900

    add checking formulae/cask spelling

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 # This software is released under the MIT License, see LICENSE.
 
 # Check Homebrew
-while read -p "Do You have Homebrew? y/n: " yn; do
+while read -p "Did you install Homebrew? y/n: " yn; do
     if [ $yn = "y" ]; then
         # Check the path of command "brew"
         which brew > /dev/null
@@ -24,19 +24,19 @@ while read -p "Do You have Homebrew? y/n: " yn; do
 done
 
 # Terminate execution when an error is occured
-set -e -o pipefail
-trap 'echo "ERROR: line $LINENO, exit status = $?." >&2; exit 1' ERR
+# set -e -o pipefail
+# trap 'echo "ERROR: line $LINENO, exit status = $?." >&2; exit 1' ERR
 
 # Read BrewList & delete blank line and comment-out line & make BrewList list
 all_list=(`cat BrewList | grep -vE "^$|#"`)
 
-# Check spelling "--formulae"
+# Check spelling of "--formulae"
 if [ ${all_list[0]} != "--formulae" ]; then
     echo "'--formulae' is NOT written. Exit."
     exit
 fi
 
-# Check spelling "--casks"
+# Check spelling of "--casks"
 flag=0
 for ((idx=0; idx<${#all_list[@]}; idx++)); do
     if [ ${all_list[$idx]} == "--casks" ]; then
@@ -71,34 +71,97 @@ cask_list=(${cask_list[@]})
 
 # Install formulae
 for formulae in ${formulae_list[@]}; do
-    echo "\\nInstalling $formulae..."
-    brew install $formulae
+    printf "\\nSearching $formulae... "
+    brew search $formulae > /dev/null 2>&1
+    if [ $? -eq 1 ]; then # If couldn't find formulae
+        echo "We couldn't find '$formulae,' maybe '$formulae' is spelled wrong."
+        while read -p "Continue(c) or Exit(e)? c/e: " ce; do
+            if [ $ce = "c" ]; then
+                break
+            elif [ $ce = "e" ]; then
+                exit
+            else
+                echo "Enter c or e."
+            fi
+        done
+    else # If can find formulae
+        printf "OK\\n"
+        echo "Installing $formulae..."
+        brew install $formulae
+    fi
 done
 
 # Install casks
 for cask in ${cask_list[@]}; do
-    echo "\\nInstalling $cask..."
-    brew install --cask $cask
+    printf "\\nSearching $cask..."
+    brew search $cask > /dev/null 2>&1
+    if [ $? -eq 1 ]; then # If couldn't find cask
+        echo "We couldn't find '$cask,' maybe '$cask' is spelled wrong."
+        while read -p "Continue(c) or Exit(e)? c/e: " ce; do
+            if [ $ce = "c" ]; then
+                break
+            elif [ $ce = "e" ]; then
+                exit
+            else
+                echo "Enter c or e."
+            fi
+        done
+    else # If can find cask
+        printf "OK\\n"
+        echo "Installing $cask..."
+        brew install --cask $cask
+    fi
 done
 
 # Install color-scheme of iTerm2 (iceberg)
 curl -o ~/Downloads/iceberg.itermcolors -O https://raw.githubusercontent.com/aseom/dotfiles/master/osx/iterm2/iceberg.itermcolors
 
-# Create file gitconfig
+# SSH
+if [ ! -d ~/.ssh ]; then
+    chmod 755 ~
+    mkdir ~/.ssh
+    chmod 700 ~/.ssh
+fi
+while read -p "\\nDo you generate RSA-key for SSH? y/n: " yn; do
+    if [ $yn = "y" ]; then
+        cd ~/.ssh
+        echo "Generate ssh-key RSA 4096-bit."
+        ssh-keygen -b 4096 -t rsa
+        break
+    elif [ $yn = "n" ]; then
+        echo "Don't generate RSA-key."
+        break
+    else
+        echo "Enter y or n."
+    fi
+done
+
+# Create file .gitconfig
 if [ ! -e ~/.gitconfig ]; then
     touch ~/.gitconfig
 fi
 
-echo
-read -p "Enter your user name on Git: " git_user_name
-read -p "Enter your mail address on Git: " git_mail
-git config --global user.name $git_user_name
-git config --global user.email $git_mail
+# Set .gitconfig
+while read -p "Do you set .gitconfig? y/n: " yn; do
+    if [ $yn = "y" ]; then
+        echo
+        read -p "Enter your user name on Git: " git_user_name
+        read -p "Enter your mail address on Git: " git_mail
+        git config --global user.name $git_user_name
+        git config --global user.email $git_mail
+        break
+    elif [ $yn = "n" ]; then
+        echo "Don't set .gitconfig."
+        break
+    else
+        echo "Enter y or n."
+    fi
+done
 
 # .zshrc
 cp ./.zshrc ~/.zshrc
 
-# Create vimrc
+# Create .vimrc
 if [ ! -e ~/.vimrc ]; then
     touch ~/.vimrc
 fi
@@ -127,7 +190,7 @@ fi
 
 # Create Rust directory & install Rust
 echo
-while read -p "Do You install Rust? y/n: " yn; do
+while read -p "Do you install Rust? y/n: " yn; do
     if [ $yn = "y" ]; then
         if  [ ! -d ~/rust ]; then
             mkdir ~/rust
@@ -142,15 +205,5 @@ while read -p "Do You install Rust? y/n: " yn; do
         echo "Enter y or n."
     fi
 done
-
-# SSH
-if [ ! -d ~/.ssh ]; then
-    chmod 755 ~
-    mkdir ~/.ssh
-    chmod 700 ~/.ssh
-    cd ~/.ssh
-    echo "\\nGenerate ssh-key RSA 4096-bit."
-    ssh-keygen -b 4096 -t rsa
-fi
 
 echo "\\nSetting Mac is DONE! Have a good Mac-life!!"
